b00d37de13c6d9b14549c1de48d58138fd6f35b1
Ostap Danyliv <danyliv.ostap@gmail.com>
2022-05-10 13:14:07 +0300

    Spring part added to project

--- src/main/java/cinema/service/OrderService.java
+++ src/main/java/cinema/service/OrderService.java
@@ -1,12 +1,12 @@
 package cinema.service;
 
+import java.util.List;
 import cinema.model.Order;
 import cinema.model.ShoppingCart;
 import cinema.model.User;
-import java.util.List;
 
 public interface OrderService {
     Order completeOrder(ShoppingCart shoppingCart);
 
-    List<Order> getOrderHistory(User user);
+    List<Order> getOrdersHistory(User user);
 }
